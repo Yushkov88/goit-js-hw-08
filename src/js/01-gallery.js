ca49ee93bbc328d2c1fe56494d8dd058ae51f0c3
Yushkov88 <yushkov1988@gmail.com>
2022-08-17 19:41:20 +0300

    hw8 part1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  const galleryItemsHtml = galleryItems.map(
+    ({ description, original, preview }) => {
+      const galleryItem = `      
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>      
+    `;
+
+      return galleryItem;
+    }
+  );
+
+  gallery.insertAdjacentHTML('beforeend', galleryItemsHtml.join(''));
+}
+
+renderGallery();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
